502fb2ff094050e745d00a1ed979392fa3fe6caf
Nick Stupich <nick.stupich@gmail.com>
2012-06-11 18:06:32 -0400

    loop or interrupt based bit flipping working, power consumption measured at 41-49mA (pic24h vs pic33f) at 3.0 V

--- Test2/main.c
+++ Test2/main.c
@@ -1,4 +1,5 @@
-#include "p33fxxxx.h"
+//#include "p33fxxxx.h"
+#include "p24hxxxx.h"
 
 #define USING_PLL	1
 
@@ -26,6 +27,7 @@ void delay(unsigned long ms)
 	while((TMR1 - lastTime) < timeout);
 }
 
+/*
 void initTimer1()
 {
 	T1CON = 0;	//in reset state
@@ -45,8 +47,40 @@ int main()
 
 	while(1)
 	{
-		delay(10);
+		delay(100);
 		LATA = ~LATA;
 		LATB = ~LATB;
 	}
 }
+*/
+
+void InitTimer1( void )
+{
+	T1CON = 0;						/* ensure Timer 1 is in reset state */
+ 	IFS0bits.T1IF = 0;				/* reset Timer 1 interrupt flag */
+	IPC0bits.T1IP = 4;				/* set Timer1 interrupt priority level to 4 */
+ 	IEC0bits.T1IE = 1;				/* enable Timer 1 interrupt */
+	PR1 = 0xFFFF;					/* set Timer 1 period register */
+	T1CONbits.TCKPS = 0x3;			/* select Timer1 Input Clock Prescale */
+	T1CONbits.TCS = 0;			 	/* select external timer clock */
+	T1CONbits.TON = 1;			 	/* enable Timer 1 and start the count */ 	
+}
+
+int main()
+{
+	LATA  = 0x0000; 				// set latch levels
+	TRISA = 0x0000; 				// set IO as outputs
+	LATB  = 0x0000; 				// set latch levels
+	TRISB = 0x0000; 				// set IO as outputs
+	
+	InitTimer1();					// start the timer
+
+	while(1);
+}
+
+void __attribute__((interrupt, auto_psv)) _T1Interrupt( void )
+{
+	LATA = ~LATA;
+	LATB = ~LATB;
+ 	IFS0bits.T1IF = 0;				/* reset timer interrupt flag	*/
+}	
